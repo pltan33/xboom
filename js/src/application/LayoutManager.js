a79c899a30af9d49f60429964f9f82ce1e6f00e5
Atridge D'Costa <atridge.dcosta@blueflag.com.au>
2013-07-23 12:02:45 +1000

    Added layout manager

---
+++ js/src/application/LayoutManager.js
@@ -0,0 +1,38 @@
+xboom.LayoutManager = _.extend({
+    initialize: function() {
+        this.$el = $('body');
+        this._views = [];
+        this._pos = 1;
+    },
+    add: function(view) {
+        if (!this.viewExists(view)) {
+            this._views.push(view);
+            this.$el.append(view.el);
+            this.updatePos();
+        }
+    },
+    remove: function(view) {
+        this._views.pop();
+        this.updatePos();
+    },
+    viewExists: function(view) {
+        var exists = _.find(this._views, function(v) {
+            return v.cid == view.cid;
+        });
+
+        return exists ? true : false;
+    },
+    updatePos: function() {
+        var pos = this._views.length;
+        if (pos != this._pos) {
+            this.$el.removeClass('pos-' + this._pos).addClass('pos-' + pos);
+            this._pos = pos;
+        }
+    },
+    showSettings: function() {
+        this.$el.removeClass('pos-' + this._pos).addClass('pos-0');
+    },
+    hideSettings: function() {
+        this.$el.addClass('pos-' + this._pos).removeClass('pos-0');
+    }
+}, Backbone.Events);
