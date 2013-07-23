7c4413299706062169be4a2e166372a5d8a7a436
Pauline Tan <pauline.tan@blueflag.com.au>
2013-07-23 16:56:17 +1000

    IGNORE

--- js/src/movies/views/MovieView.js
+++ js/src/movies/views/MovieView.js
@@ -1,10 +1,14 @@
 xboom.Views.Movie = Backbone.View.extend({
     template: xboom.Templates['js/src/movies/templates/MovieTemplate.html'],
     initialize: function() {
-        console.log(this.model.toJSON());
+        // console.log(this.model.toJSON());
         this.render();
     },
     render: function() {
+        var data = this.model.toJSON();
+        this.$el.html(this.template(data));
+        console.log(data);
+        xboom.LayoutManager.add(this);
         return this;
     }
 });
