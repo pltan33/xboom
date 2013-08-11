4037b4f7b856532ca7d32922de59f78895f07c4c
Pauline Tan <pauline.tan@blueflag.com.au>
2013-08-11 17:08:02 +1000

    not sorry Atdy that i broke grunt

--- js/src/xboom.js
+++ js/src/xboom.js
@@ -18,7 +18,7 @@ var xboom = {
 // @include movies/views/MovieListView.js
 // @include movies/views/MovieView.js
 // @include movies/views/MovieInfoView.js
-// @include movies/views/RemoteControlView.js
+// @include remoteControl/views/RemoteControlView.js
 
 
 
